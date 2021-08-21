6cbeebc2885c26d87ecbf69960165593331b2fd0
MekonenWase <mekonen.wase@gmail.com>
2021-08-21 23:14:29 +0300

    firstcommit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Exercise 1://
+
+// function getRandomNum(){
+//     return Math.floor(Math.random()*100)
+// }
+// console.log(getRandomNum());
+
+// Exercise 2://
+
+// function getRandomNum(){
+//     return Math.floor(Math.random()*10000)
+// }
+// console.log(getRandomNum());
+
+// Exercise 3://
+
+// function getRandomNum(){
+//     return Math.floor(Math.random()*55)
+// }
+// console.log(getRandomNum());
+
+
+// Exercise 4://
+
+// function getRandomNum(userNum){
+//     return Math.floor(Math.random()*(userNum+1))
+// }
+// console.log(getRandomNum(6));
+
+// Exercise 5://
+
+// var userInput=Number(prompt("please insert a number between 0-9"));
+
+// function isNumUserEqualRandomNum(userNum){
+//     var RanNum=Math.floor(Math.random()*10);
+//     if(userNum<=9 && userNum>0){
+//         if(userNum == RanNum){
+//             return console.log("number is Equal"); 
+//         }
+//     }
+//     return console.log("number is not Equal!");
+// }
+
+// Exercise 6://
+
+// function isNumUserEqualRandomNum(userNum){
+//     var RanNum=Math.floor(Math.random()*1);
+//     if(userNum<=9 && userNum>0){
+//         if(userNum == RanNum){
+//             return console.log("number is Equal"); 
+//         }
+//     }
+//     return console.log("number is not Equal!");
+// }
+
+// Exercise 7://
+
+// var userNum=Number(prompt("please insert a number between 0-9"));
+// var userName=prompt("what is your name");
+// var randomNum=Math.floor(Math.random()*10);
+
+// function isUserNumwWin(userName,randomNum){
+//     if(userNum == randomNum){
+//         return console.log(`${userName} You Won your lucky number is:${randomNum}`);
+//     }
+//     return console.log(`${userName} You are lose!`);
+// }
+
